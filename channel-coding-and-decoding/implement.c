2bed8e1c0eee2e758604f87f6bdbfc97be5dd9ac
chris-nthu <chenwy0806@gapp.nthu.edu.tw>
2019-10-10 17:51:58 +0000

    Backup

--- channel-coding-and-decoding/implement.c
+++ channel-coding-and-decoding/implement.c
@@ -2,40 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 
-static const char ucase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+#define DATASIZE 128
 
-int generateRandomData() {
-    const size_t ucase_count = sizeof(ucase) - 1; // ucase includes terminating '\0'
-    size_t i, j;
- 
+static const char ucase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const size_t ucase_count = sizeof(ucase) - 1; // ucase includes terminating '\0'
+
+void generateRandomData() {
+    FILE* pFile;
+    size_t j;
+    static char data[DATASIZE];
+
     srand(time(NULL));
 
-    for(j = 0; j < ucase_count; j++) {
+    pFile = fopen("data.bin", "wb");
+
+    for(j = 0; j < DATASIZE; j++) {
         char random_char;
         int random_index = (double)rand() / RAND_MAX * ucase_count;
              
         random_char = ucase[random_index];
+        data[j] = random_char;
         printf("%c", random_char);
     }
+    
     printf("\n");
+
+    if(pFile) {
+        fwrite(data, 1, DATASIZE, pFile);
+    }
  
-    return 0;
+    fclose(pFile);
 }
 
 int main(int argc, char *argv[]) {
     FILE* pFile;
-    int buffer[32];
-
-    srand(time(NULL));
-
-    for(int i=0; i<32; i++)
-        buffer[i] = rand();
-
-    pFile = fopen("data.bin","wb");
-
-    fclose(pFile);
+    char buffer[128];
 
     generateRandomData();
 
+    pFile = fopen("data.bin", "rb");
+
+    fread(buffer, 1, DATASIZE, pFile);
+
+    printf("%s\n", buffer);
+
+    printf("%ld\n", sizeof(buffer));
+
+    fclose(pFile);
     return 0;
 }
