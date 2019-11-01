60d839280e4ab93ea5ea705679ab5d8f3558c082
chris-nthu <54671840+chris-nthu@users.noreply.github.com>
2019-11-01 11:48:33 +0800

    Finish CDMA en/decode

--- channel-coding-and-decoding/CDMA.m
+++ channel-coding-and-decoding/CDMA.m
@@ -1,10 +1,79 @@
 clc; clear;
 
-% Generate a 8x8 matrix	of Walsh codes (W)
-W = 0;
+% CDMA Simulation for N Transmitter/Receiver Pairs
+
+
+% data bit stream for each sender
+D = [  1  1  1 -1  1 -1 -1 -1  1  1  1 -1  1 -1 -1 -1;
+       1 -1  1  1  1 -1  1  1  1 -1  1  1  1 -1  1  1 ];
+
+   
+% generate a 8x8 matrix	of Walsh codes (W)
+C = 0;
 for i=1:(4-1)
-    W = [W, W; W, ~W];
+    C = [C, C; C, ~C];
 end
+X = size(C); X = X(1);
+Y = size(C); Y = Y(2);
+% modify 0 element in walsh code matrix to -1
+for i=1:X
+    for j=1:Y
+        if C(i, j) == 0
+            C(i, j) = -1;
+        end
+    end
+end
+ 
+ 
+% parameters
+M = length(C);       % length (number of bits) of code
+Y = size(D);              
+N = Y(1);            % number of unique senders / bit streams
+I = Y(2);            % number of bits per stream
+T = [];              % sum of all transmitted and encoded data on channel
+RECON = [];          % vector of reconstructed bits at receiver
+ 
+ 
+% show data bits and codes
+'Vector of senders to be transmitted:', D
+'Vector of walsh codes used for transmission:', C
+ 
+ 
+% encode bits and transmit
+G = zeros(I,M);
+for n = 1:N
+    Z = zeros(I,M);
+    for i = 1:I
+        for m = 1:M
+            Z(i,m) = [D(n,i)*C(n,m)];        
+        end
+    end
+    G = G + Z;
+end
+ % show channel traffic
+for i = 1:I
+    T = [ T G(i,:) ];
+end
+'Resulting traffic on the channel:', T
+ 
+ 
+% decode and reconstruct
+for n = 1:N
+    TOT = zeros(1,I);
+    R   = zeros(I,M);
+    for i = 1:I
+        for m = 1:M
+            R(i,m) = G(i,m) * C (n,m);
+            TOT(i) = TOT(i) + R (i,m);
+         end
+    end
+    RECON = [RECON ; TOT / M];
+end
+'Reconstructed data at the receiver:'
+RECON
 
 
+% data bit stream for each sender
+D = [  1  1  1 -1  1 -1 -1 -1  1  1  1 -1  1 -1 -1 -1 ; 
+       1 -1  1  1  1 -1  1  1  1 -1  1  1  1 -1  1  1 ];
 
