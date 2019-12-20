c14acd58822b0bae6cb9e0d749dfcaaa7f5bc0e2
chris-nthu <54671840+chris-nthu@users.noreply.github.com>
2019-12-20 14:11:46 +0800

    Backup

--- wireless_communication/HW4/hw4_2.m
+++ wireless_communication/HW4/hw4_2.m
@@ -4,26 +4,27 @@ Pb = zeros(4,9,4); % bit error rate
 n = 200000; % the number of input bits
 QPSKrandSignal = sign(randn(1,n)); % random QPSK random signal -1 and 1
 SNR = 1:9; % Es/No (dB)
+K = 1; % for uncorrelated Ricean fading
+s = sqrt((2*1*K)/2);
 
 for L = 1:4 % L is diversity branches
     r = zeros(L,n); % the received complex envelopes of the diversity branches
     
     for i = 1:9 % i is SNR Es/No
-        rayleigh_fading = (1/sqrt(2))*(randn(L,n)+j*randn(L,n)); % complex rayleigh fading gain
-        %rayleigh_fading = sqrt(normrnd(0,1,[L, n]).^2+j.*normrnd(0,1,[L, n]).^2);
+        rician_fading = ((s+randn(L,n))+j*(s+randn(L,n)))/(sqrt(2+2*s^2)); % complex rician fading gain
         noise = sqrt((1/(10^(i/10)))*0.5)*(randn(L,n)+j*randn(L,n));
         
         for k = 1:L
-            r(k,:) = QPSKrandSignal.*(rayleigh_fading(k,:))+noise(k,:);
+            r(k,:) = QPSKrandSignal.*(rician_fading(k,:))+noise(k,:);
             r_gain(k,:) = abs(r(k,:));
         end
         
         %-----  SC  ------%
         if L == 1
-            rd = (r./exp(j*angle(rayleigh_fading)));
+            rd = (r./exp(j*angle(rician_fading)));
         else
             [u,v] = max(r_gain);
-            rd = sum((sparse(v,[1:n],1)).*r)./exp(j*angle(sum((sparse(v,[1:n],1)).*rayleigh_fading)));
+            rd = sum((sparse(v,[1:n],1)).*r)./exp(j*angle(sum((sparse(v,[1:n],1)).*rician_fading)));
         end
         rd = sign(real(rd));
         Pb(1,i,L) = sum(rd~=QPSKrandSignal)/n; % Compare with origin transmitted signal to calculate bit error rate
@@ -31,9 +32,9 @@ for L = 1:4 % L is diversity branches
         
         %-----  MRC  -----%
         if L == 1
-            rd = r.*conj(rayleigh_fading);
+            rd = r.*conj(rician_fading);
         else
-            rd = sum(r.*conj(rayleigh_fading));
+            rd = sum(r.*conj(rician_fading));
         end   
         rd = sign(real(rd));
         Pb(2,i,L) = sum(rd~=QPSKrandSignal)/n; % Compare with origin transmitted signal to calculate bit error rate
@@ -41,9 +42,9 @@ for L = 1:4 % L is diversity branches
         
         %-----  EGC  -----%
         if L == 1
-            rd = (r./exp(j*angle(rayleigh_fading)));
+            rd = (r./exp(j*angle(rician_fading)));
         else
-            rd = sum(r./exp(j*angle(rayleigh_fading)));
+            rd = sum(r./exp(j*angle(rician_fading)));
         end   
         rd = sign(real(rd));
         Pb(3,i,L) = sum(rd~=QPSKrandSignal)/n; % Compare with origin transmitted signal to calculate bit error rate
@@ -63,24 +64,24 @@ end
 
 figure(1);
 semilogy(SNR,Pb(1,:,1),SNR,Pb(1,:,2),SNR,Pb(1,:,3),SNR,Pb(1,:,4));
-title('QPSK Selective Combining (Rayleigh)');
+title('QPSK Selective Combining (Rician)');
 xlabel('SNR(dB)');ylabel('P_b');
 legend('L=1','L=2','L=3','L=4');ylim([10^-4 10^0]);
 
 figure(2);
 semilogy(SNR,Pb(2,:,1),SNR,Pb(2,:,2),SNR,Pb(2,:,3),SNR,Pb(2,:,4));
-title('QPSK Maximal Ratio Combining (Rayleigh)');
+title('QPSK Maximal Ratio Combining (Rician)');
 xlabel('SNR(dB)');ylabel('P_b');
 legend('L=1','L=2','L=3','L=4');ylim([10^-4 10^0]);
 
 figure(3);
 semilogy(SNR,Pb(3,:,1),SNR,Pb(3,:,2),SNR,Pb(3,:,3),SNR,Pb(3,:,4));
-title('QPSK Equal Gain Combining (Rayleigh)');
+title('QPSK Equal Gain Combining (Rician)');
 xlabel('SNR(dB)');ylabel('P_b');
 legend('L=1','L=2','L=3','L=4');ylim([10^-4 10^0]);
 
 figure(4);
 semilogy(SNR,Pb(4,:,1),SNR,Pb(4,:,2),SNR,Pb(4,:,3),SNR,Pb(4,:,4));
-title('QPSK Direct Combining (Rayleigh)');
+title('QPSK Direct Combining (Rician)');
 xlabel('SNR(dB)');ylabel('P_b');
 legend('L=1','L=2','L=3','L=4');ylim([10^-4 10^0]);
